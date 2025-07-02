69aaba2345ec880dbb1d12e00e7fe6ffcb7ce25f
srikanth <srikanth@descript.com>
2025-07-02 18:29:06 +0000

    More

--- compat/cuda/cuda_runtime.h
+++ compat/cuda/cuda_runtime.h
@@ -182,6 +182,9 @@ static inline __device__ float fabsf(float a) { return __builtin_fabsf(a); }
 static inline __device__ float fabs(float a) { return __builtin_fabsf(a); }
 static inline __device__ double fabs(double a) { return __builtin_fabs(a); }
 static inline __device__ float sqrtf(float a) { return __builtin_sqrtf(a); }
+static inline __device__ float fmaxf(float a, float b) { return __builtin_fmaxf(a, b); }
+static inline __device__ float fminf(float a, float b) { return __builtin_fminf(a, b); }
+static inline __device__ float powf(float a, float b) { return __powf(a, b); }
 
 static inline __device__ float __saturatef(float a) { return __nvvm_saturate_f(a); }
 static inline __device__ float __sinf(float a) { return __nvvm_sin_approx_f(a); }
